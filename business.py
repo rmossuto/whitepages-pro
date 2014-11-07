bb97fcff6f38105c7d1fb0ac1f42e9c414ee61f6
rmossuto <rmossuto@outlook.com>
2014-11-07 08:41:29 -0800

    Initial commit

---
+++ business.py
@@ -0,0 +1,12 @@
+import urllib2
+import urllib
+# Specify the url
+url = 'https://proapi.whitepages.com/2.0/business.json?name=Toyota&city=Seattle&api_key=60994ffaf2b89f76b1bd54c33c530403'
+# Package Request
+request = urllib2.Request(url)
+# Send the request and retrieve the response
+response = urllib2.urlopen(request)
+# Extract the response
+html = response.read()
+# Display response
+print html
